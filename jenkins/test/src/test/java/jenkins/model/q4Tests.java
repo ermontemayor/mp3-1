9cf6588eecb0d134f65211df4453f2a16feb3c9e
jglu224 <jglu224@uky.edu>
2019-03-27 20:58:45 -0400

    test file for q4

--- jenkins/test/src/test/java/jenkins/model/q4Tests.java
+++ jenkins/test/src/test/java/jenkins/model/q4Tests.java
@@ -7,8 +7,24 @@ import org.junit.Test;
 public class q4Tests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDollarSign() {
+		String filename = "abcd$efghi";
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		assertEquals(test.idFromFilename(filename), "abcdefghi");
+	}
+	
+	@Test 
+	public void testBaseCase() {
+		String filename = "[a-z0-9_. -]+";
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		assertEquals(test.idFromFilename(filename), "[a-z0-9_. -]+");
+	}
+	
+	@Test
+	public void testDollarSignAtEnd() {
+		String filename = "abcd$ef";
+		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
+		assertEquals(test.idFromFilename(filename), "abcdef");
 	}
 
 }
