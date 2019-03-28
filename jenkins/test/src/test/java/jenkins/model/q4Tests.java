bb8830c9dd4fe576fc8423a5ad76c8b2e42bd0c8
jglu224 <jglu224@uky.edu>
2019-03-27 20:48:24 -0400

    test file for q4

---
+++ jenkins/test/src/test/java/jenkins/model/q4Tests.java
@@ -0,0 +1,14 @@
+package jenkins.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class q4Tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
