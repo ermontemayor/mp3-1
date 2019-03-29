f9791fc085d6e0ae786612ba4c2d18d6c83acbef
jglu224 <47038069+jglu224@users.noreply.github.com>
2019-03-29 16:33:25 -0400

    Update q4Tests.java

--- jenkins/core/src/test/java/jenkins/model/q4Tests.java
+++ jenkins/core/src/test/java/jenkins/model/q4Tests.java
@@ -8,23 +8,24 @@ public class q4Tests {
 
 	@Test
 	public void testDollarSign() {
-		String filename = "abcd$efghi";
+		String filename = "abcd$0041";
 		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
-		assertEquals(test.idFromFilename(filename), "abcdefghi");
+		assertEquals(test.idFromFilename(filename), "abcdeA");
 	}
 	
+	/*
 	@Test 
 	public void testBaseCase() {
 		String filename = "[a-z0-9_. -]+";
 		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
 		assertEquals(test.idFromFilename(filename), "[a-z0-9_. -]+");
-	}
+	}*/
 	
 	@Test
 	public void testDollarSignAtEnd() {
-		String filename = "abcd$ef";
+		String filename = "abcd$002A";
 		IdStrategy.CaseSensitive test = new IdStrategy.CaseSensitive();
-		assertEquals(test.idFromFilename(filename), "abcdef");
+		assertEquals(test.idFromFilename(filename), "abcd*");
 	}
 
 }
